c51f01ef558ac28558105a416f4179dc6750fa9f
Arnaud Flisch <arnaud.flisch@outlook.com>
2025-09-26 10:55:47 +0200

    update apiurl import

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -11,8 +11,9 @@ export default function Home() {
   const [error, setError] = useState<string | null>(null);
 
   const sendApiRequestandHandleError = async (method: string = 'GET', path: string, body?: any) => {
+    const apiUrl = import.meta.env.VITE_API_URL;
     try {
-      const response = await fetch(`http://localhost:3000/api/${path}`, {
+      const response = await fetch(`${apiUrl}/api/${path}`, {
         method: method,
         headers: body ? { 'Content-Type': 'application/json' } : {},
         body: body ? JSON.stringify(body) : null,
